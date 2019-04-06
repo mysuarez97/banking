c972631a9727bcc1daa9c7c771328c762dadf47c
SamGraham34 <47090256+SamGraham34@users.noreply.github.com>
2019-04-06 12:45:55 -0400

    upload project files

---
+++ banking/src/main/java/com/banking/SavingsAccount.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.banking;
+
+import static com.banking.TransactionType.*;
+
+/**
+ * @author wpollock
+ *
+ */
+public class SavingsAccount extends Account {
+    // Default monthly interest rate, applied on last day of statement cycle:
+    private static double DEFAULT_INTEREST_RATE = 2.0;  // a percent
+
+    /**  SavingsAccount constructor, using default interest rate.
+     * @param cust The customer that owns this account
+     * @param initialBalance The initial account balance
+     * @param description An account description provided by the owner
+     */
+    public SavingsAccount (Customer cust, double initialBalance,
+            String description) {
+        super(cust, initialBalance, description);
+    }
+
+    /** SavingsAccount constructor, using a custom interest rate.
+     * @param cust The customer that owns this account
+     * @param initialBalance The initial account balance
+     * @param description An account description provided by the owner
+     * @param interestRate  Account's default monthly interest rate
+     */
+    public SavingsAccount (Customer cust, double initialBalance,
+            String description, double interestRate) {
+        super(cust, initialBalance, description);
+        // Stub
+    }
+
+    @Override
+    public void deposit (double amount) {
+        // Stub
+    }
---
+++ banking/src/main/java/com/banking/TransactionType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.banking;
+
+
+/** A list of the possible types of transactions in the banking simulation.
+ * @author wpollock
+ *
+ */
+public enum TransactionType {
+    DEPOSIT, WITHDRAWAL, INTEREST, CHECK, FEE, PENALTY, ADJUSTMENT;
+}
+
