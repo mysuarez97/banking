7c21043556086e317127ccbfea76531d97e98e6e
mysuarez97 <myrlene97suarez@gmail.com>
2019-04-16 11:21:27 -0400

    Add files via upload

--- banking/src/main/java/com/banking/Customer.java
+++ banking/src/main/java/com/banking/Customer.java
@@ -6,7 +6,13 @@
 package com.banking;
 
 import java.util.*;
+import org.junit.*;
 
+import javax.swing.*;
+import javax.swing.text.Element;
+
+import static java.lang.Math.pow;
+import static org.junit.Assert.*;
 /**
  * @author wpollock
  *
@@ -18,6 +24,17 @@ public class Customer implements Comparable<Customer> {
     private final String customerId;
     private final String lastName;
     private final String firstName;
+    private  JTextField CustomerID;
+    private JTextField LastName;
+    private JTextField FirstName;
+    private JLabel customerIDText;
+    private JLabel lastNameText;
+    private JLabel firstNameText;
+    private JFrame customerWindow;
+    private JFrame addAccount;
+    private JFrame signInWindow;
+    private JLabel alreadyTaken;
+
     private final SortedSet<Account> customerAccounts = new TreeSet<>();
 
     /** Creates a new Customer object from a name.
@@ -26,41 +43,76 @@ public class Customer implements Comparable<Customer> {
      * @param lastName The last name of the account owner.
      * @param firstName The first name of the account owner.
      */
+
     public Customer (Bank bank, String lastName, String firstName) {
         this.bank = null;
         this.customerId = "";
         this.lastName = "";
         this.firstName = "";
-        // Stub
+        // Stu
     }
 
     /**
      * @return the bank
      */
-    public Bank getBank () {
-        return null;  // Stub
+    @Test
+    public Bank getBank ()
+    {
+
+        return bank;
+        // Stub
     }
 
     /** Getter for customer's ID
      * @return The customer's ID
      */
-    public String getCustomerId () {
-        return null;  // Stub
+    @Test
+    public String getCustomerId ()
+    {
+        customerWindow = new JFrame("CustomerEntry");
+        customerWindow.setSize(600,700); customerWindow.setVisible(true); customerWindow.setLayout(null);
+        customerIDText = new JLabel("Customer ID:");
+        CustomerID.setVisible(true); CustomerID.setBounds(150,300,100,50); customerWindow.add(CustomerID);
+        JTextField IDNumField = new JTextField();
+        IDNumField.setVisible(true); IDNumField.setBounds(300,300,200,100); IDNumField.add(CustomerID);
+
+        if (IDNumField.isValid() ) {
+            int value = getIDNumField();
+            CustomerID.setText(Integer.toString(value));
+
+        }
+
+
+        return IDNumField;  // Stub
     }
 
     /** Getter for the customer's last name
      * @return The customer's last name
      */
+    @Test
     public String getLastName () {
-        return null;  // Stub
+        //Label
+        lastNameText = new JLabel("Last Name:");
+        lastNameText.setVisible(true);lastNameText.setBounds(150,400,100,50);customerWindow.add(lastNameText);
+        //Textfield
+        LastName = new JTextField(15); LastName.setVisible(true); LastName.setBounds(300,400,200,100);
+        customerWindow.add(LastName);
+
+        return LastName;  // Stub
     }
 
     /** Getter for the customer's first name
      * @return The customer's first name
      */
-
+    @Test
     public String getFirstName () {
-        return null;  // Stub
+        //Label
+        firstNameText = new JLabel("First Name:");
+        firstNameText.setVisible(true);firstNameText.setBounds(150,450,100,50); customerWindow.add(firstNameText);
+        //TextField
+        FirstName = new JTextField(15);FirstName.setBounds(450,300,200,100); FirstName.setVisible(true);
+        customerWindow.add(FirstName);
+        return FirstName;  // Stub
     }
 
     /** Returns a read-only SortedSet of the customer's active
@@ -69,8 +121,15 @@ public class Customer implements Comparable<Customer> {
      * @return an immutable SortedSet of accounts (check for
      *  immutability of accounts; use a List?)
      */
+    @Test
     public static SortedSet<Account> getCustomerAccounts () {
-        return null;  // Stub
+        List<Element> customerAccounts = new ArrayList<>();
+
+        //Add the customer accounts to the list
+        for(int i =0; i < customerAccounts; i++) {
+            customerAccounts.add(IDNumField);
+        }
+        return customerAccounts;  // Stub
     }
 
     /** Returns the total fees (including penalties) paid by this customer
@@ -78,15 +137,22 @@ public class Customer implements Comparable<Customer> {
      *
      * @return YTD fees paid
      */
-    public double ytdFees () {
-        return 0.0;  // Stub
+    @Test
+    public double ytdFees ( double InterPayment,
+            double faceValue,
+            double Price,
+            double Maturity) {
+        double yieldFees = (InterPayment + (faceValue-Price))/ ((faceValue+Price)/2);
+        return yieldFees;  // Stub
     }
 
     /** Returns the total interest paid to this customer for year-to-date
      * @return YTD interest payed
      */
-    public double ytdInterest () {
-        return 0.0;  // Stub
+    @Test
+    public double ytdInterest (double annualRate, double numOfComp) {
+        double yieldInterest = pow((1+(annualRate/numOfComp)),numOfComp)-1;
+        return yieldInterest;  // Stub
     }
 
     /** Adds a new bank account
@@ -94,16 +160,50 @@ public class Customer implements Comparable<Customer> {
      * @param desc A description for the account, chosen by the customer
      * @return the newly added account object
      */
-    public SavingsAccount addSavingsAccount (double initBal,  String desc) {
-        return null;  // Stub
+    @Test
+    public SavingsAccount addSavingsAccount (double initBal,  String[] desc) {
+        addAccount = new JFrame("Create Savings Account");
+        addAccount.setVisible(true);addAccount.setSize(600,700);addAccount.setLayout(null);
+        //Balance Label
+        JLabel Balance = new JLabel("Enter Balance");
+        Balance.setVisible(true); Balance.setBounds(200,300,100,50); addAccount.add(Balance);
+        //Balance TextField
+        JTextField balanceField = new JTextField(15);
+        balanceField.setVisible(true);balanceField.setBounds(300,300,200,60); addAccount.add(balanceField);
+        initBal= Double.parseDouble(balanceField.getText());
+        //Description Label
+        JLabel descripText = new JLabel("Description for Account:");
+        descripText.setVisible(true);descripText.setBounds(200,400,100,50); addAccount.add(descripText);
+        //Description DropBox
+        String [] descr = {"Savings","Checking","Retirement","Certificate Deposit"};
+
+        JComboBox descComboBox = new JComboBox(descr);
+        descComboBox.setSelectedIndex(4);
+        descComboBox.setVisible(true); descComboBox.setBounds(200,500,200,100); addAccount.add(descComboBox);
+
+
+
+
+        return addSavingsAccount(initBal,desc);  // Stub
     }
 
     /** Deletes a given account (in the real world, just marks it as defunct
      *  or something)
      * @param accountId the ID of the account to remove
      */
+    @Test
     public void removeAccount (String accountId) {
-        // Stub
+
+        accountId[CustomerID - 1] = null;
+        if (accountId != null)
+        {
+            System.out.println("Account " + accountId.getAccountId() + " has been closed with balance: " + accountId.getBalance());
+        }
+        else
+        {
+            //user input for account nr from array
+            System.out.println("Account does not exist.");
+        }
     }
 
     /** Find an account given an account ID
@@ -111,27 +211,50 @@ public class Customer implements Comparable<Customer> {
      * @param accountId The ID of the desired account
      * @return The Account object, or null if no such account
      */
+    @Test
     public Account getAccount (String accountId) {
-        return null;  // Stub
+        signInWindow = new JFrame("Sign in");
+        signInWindow.setVisible(true);signInWindow.setSize(600,700); signInWindow.setLayout(null);
+        //account ID text
+        JLabel accountIDText = new JLabel("Account ID Number:");
+        accountIDText.setVisible(true);accountIDText.setBounds(200,200,100,50);signInWindow.add(accountIDText);
+        //account Textfield
+        JTextField accountField = new JTextField(15);
+        accountField.setVisible(true);accountField.setBounds(300,300,200,60); signInWindow.add(accountField);
+        accountId= accountId.parseInteger(accountField.getText());
+        //sign in button
+        JButton signIn = new JButton("Sign In");
+        signIn.setVisible(true); signIn.setBounds(250,400,50,50);signInWindow.add(signIn);
+
+        return getAccount(accountId);  // Stub
     }
 
-    @Override
+    @Override @Test
     public String toString () {
         return "";  // Stub
     }
 
-    @Override
+    @Override @Test
     public int hashCode () {
-        return 0;  // Stub
+        int hash;
+        hash += CustomerID;
+        return hash;  // Stub
     }
 
-    @Override
+    @Override @Test
     public boolean equals (Object obj) {
+
+
         return false;  // Stub
     }
 
-    @Override
+    @Override @Test
     public int compareTo (Customer other) {
-        return 1;  // Stub
+        if(other == CustomerID)
+        {
+            alreadyTaken= new JLabel("account ID number is already taken");
+        }
+        alreadyTaken.setVisible(true);alreadyTaken.setBounds(400,500,100,100);signInWindow.add(alreadyTaken);
+        return alreadyTaken;  // Stub
     }
 }
--- banking/src/main/java/com/banking/SavingsAccount.java
+++ banking/src/main/java/com/banking/SavingsAccount.java
@@ -6,7 +6,11 @@
 package com.banking;
 
 import static com.banking.TransactionType.*;
+import org.junit.*;
 
+import javax.swing.*;
+
+import static org.junit.Assert.*;
 /**
  * @author wpollock
  *
@@ -14,15 +18,26 @@ import static com.banking.TransactionType.*;
 public class SavingsAccount extends Account {
     // Default monthly interest rate, applied on last day of statement cycle:
     private static double DEFAULT_INTEREST_RATE = 2.0;  // a percent
+    private JTextField  depositField;
+    private JFrame DepostWindow;
+    private JLabel depositText;
+    private JFrame withdrawWindow;
+    private JLabel withdrawText;
+    private JTextField withdrawField;
 
     /**  SavingsAccount constructor, using default interest rate.
      * @param cust The customer that owns this account
      * @param initialBalance The initial account balance
      * @param description An account description provided by the owner
      */
+
     public SavingsAccount (Customer cust, double initialBalance,
             String description) {
         super(cust, initialBalance, description);
+        System.out.println(getAccountId());
+        System.out.println(getBalance());
+        System.out.println(getAccountDescription());
+
     }
 
     /** SavingsAccount constructor, using a custom interest rate.
@@ -31,19 +46,58 @@ public class SavingsAccount extends Account {
      * @param description An account description provided by the owner
      * @param interestRate  Account's default monthly interest rate
      */
+
     public SavingsAccount (Customer cust, double initialBalance,
             String description, double interestRate) {
         super(cust, initialBalance, description);
         // Stub
+        System.out.println(getAccountId());
+        System.out.println(getBalance());
+        System.out.println(getAccountDescription());
     }
 
-    @Override
+    @Override @Test
     public void deposit (double amount) {
         // Stub
+        DepostWindow= new JFrame("Deposit");
+        DepostWindow.setVisible(true); DepostWindow.setSize(500,600);DepostWindow.setLayout(null);
+        //Deposit Label
+        depositText= new JLabel("Enter the amount, you want to deposit.");
+        depositText.setVisible(true);depositText.setBounds(200,200,100,50);DepostWindow.add(depositText);
+        //TextField
+        depositField = new JTextField(15);
+        depositField.setVisible(true);depositField.setBounds(300,300,200,60); DepostWindow.add(depositField);
+
+         amount =  depositField.getColumns();
+
+
+        if(amount <= 0){
+            System.out.println("You must deposit an amount greater than 0.");
+        } else {
+            System.out.println("You have deposited " + (amount));
+        }
     }
 
-    @Override
+    @Override @Test
     public void withdraw(double amount) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        withdrawWindow = new JFrame("Withdraw");
+        withdrawWindow.setVisible(true); withdrawWindow.setSize(500,600);withdrawWindow.setLayout(null);
+        //Deposit Label
+        withdrawText= new JLabel("Enter the amount, you want to withdraw from your account");
+        withdrawText.setVisible(true);withdrawText.setBounds(200,200,100,50);withdrawWindow.add(withdrawText);
+        //TextField
+        withdrawField = new JTextField(15);
+        withdrawField.setVisible(true);withdrawField.setBounds(300,300,200,60); withdrawWindow.add(withdrawField);
+
+        amount =  depositField.getColumns();
+
+        if(amount <= 0) {
+            System.out.println("You must withdraw an amount greater than 0.");
+        } else {
+            System.out.println("You have deposited " + (amount));
+
+        }
     }
 }
